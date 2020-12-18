a50e03f2eeabde866faffb4287add35e54d47c2a
shravaniallamreddy <shravani.allam@gmail.com>
2020-12-18 13:11:50 -0700

    changed icons

--- public/service-worker.js
+++ public/service-worker.js
@@ -10,6 +10,7 @@ const FILES_TO_CACHE = [
     '/db.js',
     '/icons/icon-192x192.png',
     '/icons/icon-512x512.png',
+    '/icons/icon-144x144.png',
     'https://cdn.jsdelivr.net/npm/chart.js@2.8.0',
     'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
     'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/fonts/fontawesome-webfont.woff2?v=4.7.0'
